4a65da3fea2fc6510ed4acd5a1bfabf4f8eabb93
Simon Pocock <32938450+srpocock@users.noreply.github.com>
2025-08-08 20:56:51 +0100

    Linting

--- keyworder-frontend/src/components/Game.tsx
+++ keyworder-frontend/src/components/Game.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from 'react';
 import words from '../utils/words.ts';
 import { GameState, type GameStateType } from '../utils/GameState.ts';
 import Instructions from './Instructions.tsx';
@@ -19,7 +19,7 @@ export default function Game() {
     const [gameState, setGameState] = useState<GameStateType>(GameState.GuessingPairs);
 
     // Handle what happens when word tiles are selected
-    function handleCheck (word: string, checked: boolean): void {
+    function handleCheck(word: string, checked: boolean): void {
         // Make sure only a maximum of 2 words are checked
         if (checked && selectedWords.length >= 2) {
             return;
@@ -30,23 +30,23 @@ export default function Game() {
     };
 
     // Handle what happens when submit button is pressed
-    function handleSubmit (): void {
+    function handleSubmit(): void {
 
         if (gameState === GameState.GuessingPairs) {
             const pairValue = words.isPair(selectedWords);
-        
+
             if (pairValue === -1) {
                 // Returns -1 if not pairs
                 setAttemptsRemaining(prev => prev - 1);
             } else {
                 // Otherwise store wordStates and reset for more guesses if needed
-                setWordStates((prev) => ({...prev,
+                setWordStates((prev) => ({
+                    ...prev,
                     [selectedWords[0]]: pairValue,
                     [selectedWords[1]]: pairValue
                 }));
 
                 setSelectedWords([]);
-
 
             }
         } else if (gameState === GameState.GuessingKeyword) {
@@ -55,14 +55,14 @@ export default function Game() {
 
     }
 
-    useEffect(() => { 
-        if(attemptsRemaining <= 0) { 
+    useEffect(() => {
+        if (attemptsRemaining <= 0) {
             setGameState(GameState.Lost);
             setSelectedWords([]);
         }
     }, [attemptsRemaining]);
 
-    useEffect(() => { 
+    useEffect(() => {
         if (Object.values(wordStates).findIndex((val) => val === -1) === -1) {
             setGameState(GameState.GuessingKeyword);
         }
@@ -70,11 +70,11 @@ export default function Game() {
 
     return (
         <article className="game">
-            <Instructions gameState={gameState}/>
-            <WordGrid gameState={gameState} wordStates={wordStates} selectedWords={selectedWords} onChecked={handleCheck}/>
-            <Attempts attemptsRemaining={attemptsRemaining}/>
+            <Instructions gameState={gameState} />
+            <WordGrid gameState={gameState} wordStates={wordStates} selectedWords={selectedWords} onChecked={handleCheck} />
+            <Attempts attemptsRemaining={attemptsRemaining} />
             <KeywordInput />
-            <Buttons numSelectedWords={selectedWords.length} onSubmit={handleSubmit}/>
+            <Buttons numSelectedWords={selectedWords.length} onSubmit={handleSubmit} />
             <History />
         </article>
     )
